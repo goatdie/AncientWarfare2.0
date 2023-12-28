3606a19bad8a7a9a19dcbfcfc84486da5e229e49
inmny <1342885270@qq.com>
2023-12-28 16:42:51 +0800

    Repo: 局部代码格式化

--- Code/core/kingdom_policies/KingdomPolicyAsset.cs
+++ Code/core/kingdom_policies/KingdomPolicyAsset.cs
@@ -7,61 +7,61 @@ public delegate KingdomPolicyAsset FindPolicy(AW_Kingdom kingdom, KingdomPolicyD
 
 public class KingdomPolicyStateAsset : Asset
 {
-    /// <summary>
-    /// 所有当前状态类型可选的政策
-    /// </summary>
-    public HashSet<string> all_optional_policies;
-    /// <summary>
-    ///     查找下一个政策的方法
-    /// </summary>
-    public FindPolicy policy_finder;
+  /// <summary>
+  ///   所有当前状态类型可选的政策
+  /// </summary>
+  public HashSet<string> all_optional_policies;
+  /// <summary>
+  ///   查找下一个政策的方法
+  /// </summary>
+  public FindPolicy policy_finder;
 }
 
 public class KingdomPolicyAsset : Asset
 {
-    /// <summary>
-    /// 所有前置政策
-    /// </summary>
-    public HashSet<string> all_prepositions;
-    /// <summary>
-    /// 所有分支政策
-    /// </summary>
-    public HashSet<string> branches;
-    /// <summary>
-    ///     在计划阶段的花费时间
-    /// </summary>
-    public int cost_in_plan = 100;
-    /// <summary>
-    ///     在执行阶段的花费时间
-    /// </summary>
-    public int cost_in_progress = 100;
-    /// <summary>
-    /// 描述文本的key
-    /// </summary>
-    public string description;
-    /// <summary>
-    /// 执行政策的相关逻辑行为
-    /// </summary>
-    public ExecutePolicy execute_policy;
+  /// <summary>
+  ///   所有前置政策
+  /// </summary>
+  public HashSet<string> all_prepositions;
+  /// <summary>
+  ///   所有分支政策
+  /// </summary>
+  public HashSet<string> branches;
+  /// <summary>
+  ///   在计划阶段的花费时间
+  /// </summary>
+  public int cost_in_plan = 100;
+  /// <summary>
+  ///   在执行阶段的花费时间
+  /// </summary>
+  public int cost_in_progress = 100;
+  /// <summary>
+  ///   描述文本的key
+  /// </summary>
+  public string description;
+  /// <summary>
+  ///   执行政策的相关逻辑行为
+  /// </summary>
+  public ExecutePolicy execute_policy;
 
-    public string path_icon;
-    /// <summary>
-    /// 政策名称的key
-    /// </summary>
-    public string policyname;
-    public Kingdom special_kingdom; //给特定国家的特殊国策
+  public string path_icon;
+  /// <summary>
+  ///   政策名称的key
+  /// </summary>
+  public string policyname;
+  public Kingdom special_kingdom; //给特定国家的特殊国策
 }
 
 public class KingdomPolicyGraphAsset : Asset
 {
-    /// <summary>
-    /// 所有政策
-    /// </summary>
-    public HashSet<KingdomPolicyAsset> all_policies;
-    /// <summary>
-    /// 所有状态
-    /// </summary>
-    public HashSet<KingdomPolicyStateAsset> all_states;
+  /// <summary>
+  ///   所有政策
+  /// </summary>
+  public HashSet<KingdomPolicyAsset> all_policies;
+  /// <summary>
+  ///   所有状态
+  /// </summary>
+  public HashSet<KingdomPolicyStateAsset> all_states;
 }
 
 // 三个类继承自library
--- Code/core/kingdom_policies/KingdomPolicyStateLibrary.cs
+++ Code/core/kingdom_policies/KingdomPolicyStateLibrary.cs
@@ -2,22 +2,22 @@ namespace Figurebox;
 
 public class KingdomPolicyStateLibrary : AssetLibrary<KingdomPolicyStateAsset>
 {
-    public static KingdomPolicyStateLibrary Instance { get; } = new();
-    public static KingdomPolicyStateAsset DefaultState { get; } = new();
-    public override void init()
+  public static KingdomPolicyStateLibrary Instance { get; } = new();
+  public static KingdomPolicyStateAsset DefaultState { get; } = new();
+  public override void init()
+  {
+    base.init();
+    DefaultState.id = "default";
+    DefaultState.policy_finder = (pKingdom, pPolicyData, pState) =>
     {
-        base.init();
-        DefaultState.id = "default";
-        DefaultState.policy_finder = (pKingdom, pPolicyData, pState) =>
-        {
-            var policy = KingdomPolicyLibrary.Instance.get("hello_world");
-            if (policy == null)
-            {
-                Main.LogWarning("政策'hello_world'不存在, 终止", true);
-                return null;
-            }
-            return policy;
-        };
-        add(DefaultState);
-    }
+      var policy = KingdomPolicyLibrary.Instance.get("hello_world");
+      if (policy == null)
+      {
+        Main.LogWarning("政策'hello_world'不存在, 终止", true);
+        return null;
+      }
+      return policy;
+    };
+    add(DefaultState);
+  }
 }
