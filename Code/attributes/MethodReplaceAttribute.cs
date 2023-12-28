0cafc5aa2ad046c260350d8a9282da6890ac0394
inmny <1342885270@qq.com>
2023-12-28 21:00:00 +0800

    Feature: 函数替换

--- Code/attributes/MethodReplaceAttribute.cs
+++ Code/attributes/MethodReplaceAttribute.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Reflection;
+using HarmonyLib;
 namespace Figurebox.attributes;
 
 public class MethodReplaceAttribute : Attribute
@@ -9,6 +11,10 @@ public class MethodReplaceAttribute : Attribute
     public MethodReplaceAttribute()
     {
 
+    }
+    public MethodReplaceAttribute(string pMethodName)
+    {
+        MethodName = pMethodName;
     }
     public MethodReplaceAttribute(Type pTargetType)
     {
@@ -21,6 +27,38 @@ public class MethodReplaceAttribute : Attribute
     }
     public MethodInfo TrackTargetMethod(MethodInfo pOnMethod)
     {
-        throw new NotImplementedException();
+        List<Type> parameter_types = new();
+        foreach (var parameter in pOnMethod.GetParameters())
+        {
+            parameter_types.Add(parameter.ParameterType);
+        }
+        List<Type> generic_types = new();
+        foreach (var generic_type in pOnMethod.GetGenericArguments())
+        {
+            generic_types.Add(generic_type);
+        }
+
+        var parameter_types_array = parameter_types.ToArray();
+        var generic_types_array = generic_types.Count == 0 ? null : generic_types.ToArray();
+
+        Type target_type = TargetType;
+
+        var target_method_name = string.IsNullOrEmpty(MethodName) ? pOnMethod.Name : MethodName;
+
+        if (target_type != null) return AccessTools.Method(TargetType, target_method_name, parameter_types_array, generic_types_array);
+
+        if (pOnMethod.DeclaringType == null) return null;
+
+        target_type = pOnMethod.DeclaringType;
+        if (target_type.BaseType != null)
+            target_type = target_type.BaseType;
+
+        var target_method = AccessTools.Method(target_type, target_method_name, parameter_types_array, generic_types_array);
+        while (target_method == null && target_type.BaseType != null)
+        {
+            target_type = target_type.BaseType;
+            target_method = AccessTools.Method(target_type, target_method_name, parameter_types_array, generic_types_array);
+        }
+        return target_method;
     }
 }
--- Code/utils/AutoMethodReplaceTool.cs
+++ Code/utils/AutoMethodReplaceTool.cs
@@ -1,28 +1,92 @@
 using System;
+using System.Collections.Generic;
 using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
 using Figurebox.attributes;
+using HarmonyLib;
 namespace Figurebox.Utils;
 
 internal class AutoMethodReplaceTool
 {
+    private static MethodInfo _origin_method;
     public static void ReplaceMethods()
     {
         Type[] types = Assembly.GetExecutingAssembly().GetTypes();
+
+        HashSet<string> replaced_methods = new();
         foreach (Type type in types)
         {
             MethodInfo[] method_infos = type.GetMethods(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
             foreach (MethodInfo method_info in method_infos)
             {
                 var attribute = method_info.GetCustomAttribute<MethodReplaceAttribute>();
-                if (method_info.GetCustomAttribute<MethodReplaceAttribute>() != null)
-                {
+                if (method_info.GetCustomAttribute<MethodReplaceAttribute>() == null) continue;
 
+                MethodInfo target_method = attribute.TrackTargetMethod(method_info);
+
+                if (target_method != null)
+                {
+                    StringBuilder method_full_name = new();
+                    method_full_name.Append(target_method.DeclaringType.FullName);
+                    method_full_name.Append(".");
+                    method_full_name.Append(target_method.Name);
+                    method_full_name.Append("<");
+                    foreach (var parameter in target_method.GetParameters())
+                    {
+                        method_full_name.Append(parameter.ParameterType.FullName);
+                        method_full_name.Append(",");
+                    }
+                    method_full_name.Append(">");
+
+                    if (replaced_methods.Contains(method_full_name.ToString()))
+                    {
+                        Main.LogWarning("重复替换方法, 请检查MethodReplaceAttribute的参数是否正确");
+                        Main.LogWarning($"Attribute作用于: {method_info.DeclaringType.FullName}:{method_info.Name}");
+                        Main.LogWarning($"重复替换方法: {method_full_name}");
+                        continue;
+                    }
+                    try
+                    {
+                        ReplaceMethod(method_info, target_method);
+                        replaced_methods.Add(method_full_name.ToString());
+                    }
+                    catch (Exception e)
+                    {
+                        Main.LogWarning($"替换方法{method_full_name}时发生错误: {e}");
+                        Main.LogWarning(e.StackTrace);
+                    }
+                }
+                else
+                {
+                    Main.LogWarning("无法找到目标替换方法, 请检查MethodReplaceAttribute的参数是否正确");
+                    Main.LogWarning($"Attribute作用于: {method_info.DeclaringType.FullName}:{method_info.Name}");
                 }
             }
         }
     }
     private static void ReplaceMethod(MethodInfo pOriginalMethod, MethodInfo pTargetMethod)
     {
+        _origin_method = pOriginalMethod;
+        Harmony harmony = new($"Figurebox.AutoMethodReplaceTool.{pTargetMethod.DeclaringType.FullName}.{pTargetMethod.Name}");
+        harmony.Patch(pTargetMethod, transpiler: new HarmonyMethod(typeof(AutoMethodReplaceTool), nameof(_method_replace_patch)));
+    }
+    private static IEnumerable<CodeInstruction> _method_replace_patch(IEnumerable<CodeInstruction> instr)
+    {
+        var codes = new List<CodeInstruction>();
 
+        int param_count = _origin_method.GetParameters().Length + (_origin_method.IsStatic ? 0 : 1);
+        for (int i = 0; i < param_count; i++)
+        {
+            codes.Add(new CodeInstruction(OpCodes.Ldarg, i));
+        }
+        codes.Add(new CodeInstruction(OpCodes.Callvirt, _origin_method));
+        codes.Add(new CodeInstruction(OpCodes.Ret));
+
+        foreach (var code in codes)
+        {
+            Main.LogInfo(code.ToString());
+        }
+        return codes;
     }
 }
