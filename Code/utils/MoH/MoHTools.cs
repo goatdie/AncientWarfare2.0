9b867d63a1fab09f8f0858da884026fb44ab6954
Goat Die <2490895322@qq.com>
2023-12-29 00:42:53 +0800

    Change:王国值计算法

--- Code/utils/MoH/MoHTools.cs
+++ Code/utils/MoH/MoHTools.cs
@@ -1,4 +1,5 @@
 using Figurebox.core;
+using System.Collections.Generic;
 namespace Figurebox.Utils.MoH;
 
 public class MoHTools
@@ -11,17 +12,62 @@ public class MoHTools
     ///     当前天命国家是否存在
     /// </summary>
     public static bool ExistMoHKingdom => MoHKingdom != null && MoHKingdom.isAlive();
-    /// <summary>
-    ///     从存档中读取并直接缓存国家
-    /// </summary>
+
     public static void SetMoHKingdom(AW_Kingdom kingdom)
     {
         MoHKingdom = kingdom;
     }
     public static bool IsMoHKingdom(AW_Kingdom kingdom)
     {
-       return MoHKingdom == kingdom;
+        return MoHKingdom == kingdom;
     }
+    public static AW_Kingdom FindMostPowerfulKingdom(List<AW_Kingdom> kingdoms)
+    {
+        if (kingdoms == null || kingdoms.Count == 0)
+        {
+            return null; // 如果没有王国或王国列表为空，则返回null
+        }
+
+        AW_Kingdom mostPowerfulKingdom = null;
+        int highestValue = 0;
+
+        foreach (var kingdom in kingdoms)
+        {
+            int kingdomValue = CalculateKingdomValue(kingdom);
+            if (kingdomValue > highestValue)
+            {
+                highestValue = kingdomValue;
+                mostPowerfulKingdom = kingdom;
+            }
+        }
+
+        return mostPowerfulKingdom;
+    }
+
+    public static int CalculateKingdomValue(AW_Kingdom k)
+    {
+        int kingdomValue = 0;
+        int populationTotal = k.getPopulationTotal();
+        int cityCount = k.cities.Count * 100;
+        int armySize = k.getArmy();
+        int stewardship = 8;
+        if (k.king != null)
+        {
+            stewardship = k.king.GetData().stewardship * 10;
+
+
+        }
+
+        if (k.policy_data.current_state_id == "default")
+        {
+            kingdomValue = populationTotal + cityCount + armySize + stewardship;
+        }
+
+        return kingdomValue;
+    }
+    /// <summary>
+    ///     从存档中读取并直接缓存国家
+    /// </summary>
     internal static void LoadFromSave()
     {
 
