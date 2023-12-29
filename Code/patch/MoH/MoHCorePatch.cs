0f223edba7c803da7e9292acea122043e3d1cd68
Goat Die <2490895322@qq.com>
2023-12-30 00:37:25 +0800

    Change:增加天命特质给予

--- Code/patch/MoH/MoHCorePatch.cs
+++ Code/patch/MoH/MoHCorePatch.cs
@@ -17,65 +17,15 @@ internal static class MoHCorePatch
         }
     }
     [HarmonyPostfix]
-    [HarmonyPatch(typeof(Kingdom), "updateAge")]
-
-    public static void MOHUpdateAge_Postfix(Kingdom __instance)
+    [HarmonyPatch(typeof(Kingdom), "setKing")]
+    public static void setKing_mohPostfix(Kingdom __instance    ,Actor pActor)
     {
         AW_Kingdom awKingdom = __instance as AW_Kingdom;
 
         if (MoHTools.IsMoHKingdom(awKingdom))
         {
-            int index = 0;
-            if (awKingdom.getEnemiesKingdoms().Count == 0)
-            {
-                index++;
-            }
-
-            if (awKingdom.king != null)
-            {
-
-                if (awKingdom.king.hasTrait("first"))
-                {
-                    index += 3;
-                }
-                if (awKingdom.king.getAge() <= 24)
-                {
-                    index--;
-                }
-
-            }
-            Clan kclan = BehaviourActionBase<Kingdom>.world.clans.get(awKingdom.data.royal_clan_id);
-            if (kclan != null && kclan.units.Count <= 2)
-            {
-                index--;
-
-            }
-            MoHTools.ChangeMOH_Value(index);
+            pActor.addTrait("天命");
         }
     }
-    [HarmonyPostfix]
-    [HarmonyPatch(typeof(Kingdom), "updateAge")]
-
-    public static void MOH_condition_Postfix(Kingdom __instance)
-    {
-        if (MoHTools.MOH_Value >= MoHTools.MOH_UpperLimit)
-        {
-            MoHTools.SetMOH_Value(MoHTools.MOH_UpperLimit);
-        }
-        AW_Kingdom awKingdom = __instance as AW_Kingdom;
-        if (MoHTools.IsMoHKingdom(awKingdom))
-        {
-            if (MoHTools.MOH_Value >= 40)
-            {
-                //天命值在什么条件发生的事件
-            }
-            if (MoHTools.MOH_Value <= MoHTools.MOH_UnderLimit)
-            {
-                MoHTools.Clear_MoHKingdom();     
-                Debug.Log("去除天命"+awKingdom.data.name+MoHTools.MOH_Value);           //天命去除
-            }
-        }
-
-
-    }
+   
 }
