1d142a6d609415d56e251a2e47cdb095fbb848b2
inmny <1342885270@qq.com>
2024-02-01 19:33:18 +0800

    Feature: 支持快捷更换UnitGroup所属城市

--- Code/core/AW_UnitGroup.cs
+++ Code/core/AW_UnitGroup.cs
@@ -1,5 +1,7 @@
 using System.Linq;
 using Figurebox.attributes;
+using Figurebox.Utils;
+using JetBrains.Annotations;
 
 namespace Figurebox.core;
 
@@ -13,6 +15,76 @@ public class AW_UnitGroup : UnitGroup
         asset = pAsset;
         data = pData;
         data.city_id = pCity.data.id;
+    }
+
+    /// <summary>
+    ///     切换到指定城市，如果该城市已经有该组，则不会切换；如果该城市已经有该组，但是 pMerge 为 true，则会将该组合并到该城市的该组中
+    ///     <para>成员也会切换城市</para>
+    /// </summary>
+    /// <param name="pCity"></param>
+    /// <param name="pMerge"></param>
+    public void SetCity([NotNull] AW_City pCity, bool pMerge = false)
+    {
+        if (city == pCity) return;
+
+        if (pCity.groups.TryGetValue(asset.id, out AW_UnitGroup old_group) && !pMerge) return;
+        removeFromCurrentCitySimply();
+
+        units.checkAddRemove();
+        foreach (Actor unit in units)
+        {
+            city.removeUnit(unit);
+            pCity.addNewUnit(unit);
+        }
+
+        if (old_group != null && old_group != this)
+        {
+            foreach (Actor unit in units) old_group.addUnit(unit);
+        }
+        else
+        {
+            pCity.groups.Add(asset.id, this);
+            pCity.army ??= this;
+            city = pCity;
+            data.city_id = pCity.data.id;
+        }
+    }
+
+    /// <summary>
+    ///     从当前城市移除，这将导致该组不受任何城市控制，该组成员将会不属于原城市，但仍属于其国家
+    /// </summary>
+    public void RemoveFromCurrentCity()
+    {
+        if (city == null) return;
+
+        units.checkAddRemove();
+        foreach (Actor unit in units) city.removeUnit(unit, false);
+        removeFromCurrentCitySimply();
+    }
+
+    private void removeFromCurrentCitySimply()
+    {
+        var aw_city = (AW_City)city;
+        if (aw_city.army == this) aw_city.army = null;
+
+        aw_city.groups.Remove(asset.id);
+
+        if (aw_city.groups.Count > 0)
+        {
+            var max_army_count = 0;
+            AW_UnitGroup max_army = null;
+            foreach (AW_UnitGroup group in aw_city.groups.Values.Where(
+                         group => group.asset.base_max_count > max_army_count))
+            {
+                max_army = group;
+                max_army_count = group.asset.base_max_count;
+            }
+
+            aw_city.SetMainGroup(max_army);
+        }
+
+        city = null;
+        data.city_id = "";
     }
 
     public void LoadData(AW_UnitGroupData pData)
